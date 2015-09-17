25081bc0127464150b83e05babb5b44fcd815b78
Peter <peterahl@kth.se>
2015-09-17 22:00:46 +0200

    Updated with some new functions ect.

--- src/ne_simulator/sim_objects/ann_simple_agent.py
+++ src/ne_simulator/sim_objects/ann_simple_agent.py
@@ -7,11 +7,41 @@ from .empty import Empty
 from .food import Food
 
 
+_OBJECT_TO_INPUT = {Wall.SYMBOL:(1,0,0), Empty.SYMBOL:(0,1,0), Food.SYMBOL:(0,0,1)}
+
+_MAX_ENERGY = 1.0
+
+_ENERGY_LEVELS = 5
+
+_NUMBER_OF_INPUTS = len(_OBJECT_TO_INPUT) * 3 + _ENERGY_LEVELS + 1  # fields front, left, right, energy level and last action ok
+
+_OUTPUT_INDEX_TO_ACTION = {
+    0: SimObject.Action.MOVE, 
+    1: SimObject.Action.EAT, 
+    2: SimObject.Action.TURN_LEFT, 
+    3: SimObject.Action.TURN_RIGHT,
+    4: SimObject.Action.DO_NOTHING
+}
+
+def _max_index(input_vector):
+    max_value = 0
+    max_index = 0
+    for i, element in enumerate(input_vector):
+        if element > max_value:
+            max_value = element
+            max_index = i
+    return max_index
+
+def _energy_conversion(energy):
+    eneryg_index = round(energy * (_ENERGY_LEVELS - 1) / _MAX_ENERGY)
+    return [1 if i == index else 0 for i in range(_ENERGY_LEVELS)] 
+
+
 class ANNSimpleAgent(SimObject):
 
     SYMBOL = "s"
 
-    def __init__(self, weights, n_inputs, n_outputs = 5, n_layers = 4):
+    def __init__(self, weights, n_layers = 4):
         self._weights = np.array()
         self._n_nodes = len(weights)
         self._n_layers = n_layers
@@ -23,13 +53,9 @@ class ANNSimpleAgent(SimObject):
         self._direction = choice(directions)
         self._action 
         
-        self._input_dict = {'#':(1,0,0), ' ':(0,1,0), '*':(0,0,1)}
-        self._action_dict = {0:self.Action.MOVE, 
-                             1:self.Action.EAT, 
-                             2:self.Action.TURN_LEFT, 
-                             3:self.Action.TURN_RIGHT,
-                             4:self.Action.DO_NOTHING}
-    
+        self._energy = _MAX_ENERGY
+        self._binary_input = [0, ] * _NUMBER_OF_INPUTS
+
 
     def start_turn(self, objects_map):
         
@@ -41,13 +67,15 @@ class ANNSimpleAgent(SimObject):
             position.move(turn(self._direction, False)))
         right = objects_map.get_object_for_position(
             position.move(turn(self._direction, True)))
-        visible = (front, left, right)
-        n_input_attributes = len(visible)
-        n_values_per_attribute = 3
+        # visible = (front, left, right)
+        # n_input_attributes = len(visible)
+        # n_values_per_attribute = 3
         
         """create binary input vector"""
-        binary_input = [0,]*n_input_attributes*n_values_per_attribute
+        binary_input = [_OBJECT_TO_INPUT[f] for f in (front, left, right)]
+        binary_input.append(self._energy)
         
+
         for i in range(0,len(visible)):
             binary_input[i*3:(i+1)*3] = self._input_dict[visible[i]]
            
@@ -66,14 +94,10 @@ class ANNSimpleAgent(SimObject):
     def action(self):       
         return self._action
     
-    def step(self):
-        """ do somthing with the sigmoid function """
-        self._node_values = np.dot(self.weights, self._node_values)        
+    def _step(self):
+        self._node_values = expit(np.dot(self.weights, self._input_values + self._node_values))
     
-    def set_inputs(self, values):
-        self.node_values[0:self.n_inputs-1] = values
-    
-    def get_outputs(self):
+    def _get_outputs(self):
         return self.node_values[-self.n_outputs:]
     
     def setweights(self, weights):
