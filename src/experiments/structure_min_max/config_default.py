45a8f2aefdc9ef7d75c6127302a22d6168155055
merry-bits <merry-bits@users.noreply.github.com>
2015-11-10 20:01:04 +0100

    Added experiment for min-max structure evolution.

---
+++ src/experiments/structure_min_max/config_default.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+""" Run the agent once without evolution and show the labyrinth.
+
+Press enter to continue.
+"""
+from ne_simulator import SIMULATOR_CONFIGURATION, SIMULATOR_CLASS,\
+    CONFIGURATION_MAP
+from ne_simulator.maps import food_9x9
+from ne_simulator.sim_objects.ann_structure_agent import \
+    ANNStructuredAgent  # @UnusedImport register the Agent
+# from ne_simulator.objects_map import CONFIGURATION_PARAMETERS
+# from ne_simulator.sim_objects.sim_agent import SCORE_MONITOR
+
+
+def _print_step_count(sim):
+    """
+    :type sim: ne_simulator.Simulator
+    """
+    return "-" * sim._map._width + "\n" + "Step: {}\n".format(sim._step_count)
+
+
+EVOLUTION_KWDS = {
+    "wait_for_enter": True
+}
+
+SCENARIOS = [
+    {
+        SIMULATOR_CLASS: [
+            "ne_simulator.sim_mixins.StepsLimiter",
+            "ne_simulator.sim_mixins.MapPrinter",
+            "ne_simulator.Simulator"
+        ],
+        SIMULATOR_CONFIGURATION: {
+            CONFIGURATION_MAP: food_9x9,
+            "steps_limiter_steps": 200,
+            "map_printer_delimiter": _print_step_count
+        }
+    }
+]
